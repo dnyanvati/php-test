66e54f2617f4debcbd6bbd3f0e442e94f5bc2b3b
dnyanvati <46395830+dnyanvati@users.noreply.github.com>
2019-03-29 15:04:52 +0530

    Update php_code_test.php

--- php_code_test.php
+++ php_code_test.php
@@ -4,121 +4,14 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">    
     <!-- font-awsome -->
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
     <title>PHP Code Test</title>
-    <style type="text/css">
-      .choosePeople h5
-      {
-        color: #29338e;
-        font-weight: 400;
-        margin-bottom: 10px;
-      }
-      .choosePeople h5 strong
-      {
-        font-weight: 600;
-      }
-      .choosePeople h5 i
-      {
-        margin-right: 10px;
-      } 
-      .choosePeople ul li
-      {
-        list-style: none;
-        text-transform: uppercase;
-        color: #29338e;
-        line-height: 30px;
-        border-bottom: 1px solid #adabab;
-        padding: 15px 0;
-      }
-      .choosePeople ul li:last-child
-      {
-        border-bottom: 0;
-      }
-      .choosePeople ul li p
-      {
-        margin-bottom: 0;
-      }
-      .choosePeople .content i
-      {
-        margin-right: 10px; 
-      }   
-      .count 
-      {
-        display: inline-flex;
-        font-size: 25px;
-        line-height: 30px;
-        padding: 0 2px;
-        min-width: 45px;
-        text-align: right;
-        float: right;
-      }
-      .count .plus 
-      {
-        cursor: pointer; 
-        vertical-align: top;
-        color: white;
-        width: 30px;
-        height: 30px; 
-        text-align: center;
-        border-radius: 50%;
-        background-color: #eb375d !important;
-        line-height: 23px;
-      }
-      .count .minus 
-      {
-        cursor: pointer; 
-        vertical-align: top;
-        color: white;
-        width: 30px;
-        height: 30px; 
-        text-align: center;
-        border-radius: 50%;
-        background-clip: padding-box;
-        background-color: #29338e !important;
-        line-height: 23px;
-      } 
-      .count .minus:hover,
-      .count .plus:hover
-      {
-        opacity: 0.8 !important;
-      } 
-      /*Prevent text selection*/
-      .count span
-      {
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-      }
-      .count input
-      {  
-          border: 0; 
-          text-align: center;
-          font-size: 20px;
-          width: 50px;
-      }
-      .count input::-webkit-outer-spin-button,
-      .count input::-webkit-inner-spin-button {
-          -webkit-appearance: none;
-          margin: 0;
-      }
-      .count input:disabled{
-          background-color:white;
-      }
-      .people
-      {
-          border: 1px solid #adabab;
-          padding: 0 10px;
-          width: 100%;
-      }
-    </style>
   </head>
-  <body style="padding: 100px;">
-
+  <body>
       <section class="choosePeople">
        <div class="col-lg-6 col-md-6 col-sm-12 col-12">
           <h5><i class="fa fa-users"></i> Choose number of <strong>people</strong></h5>
@@ -168,65 +61,8 @@
         </ul>
        </div>
       </section>  
-
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-
-    <script type="text/javascript">
-	
-        var lookup_arr = { "room": { "min": 1, "max": 5} ,
-                            "adult": { "min": 1, "max": 4},
-                            "children": { "min": 0, "max": 3} };
-							
-        function qty_operation(field,op){
-            var current_val = parseInt($('.'+field+'_qty').val());
-            if(op == 1){
-                var max_val = lookup_arr[field]['max'];
-                if(current_val < max_val){
-                    $('.'+field+'_qty').val(current_val + 1 );
-					var estimated_adult = parseInt($('.room_qty').val()) * 4;
-					if(field == 'room'){
-						lookup_arr.adult.max = estimated_adult;
-						lookup_arr.adult.min = $('.room_qty').val();
-						lookup_arr.children.max = estimated_adult - parseInt($('.room_qty').val());
-						if(parseInt($('.room_qty').val()) > parseInt($('.adult_qty').val())){
-							$('.adult_qty').val(current_val + 1 );
-						}
-					}else if(field == 'adult'){
-						var total = parseInt($('.adult_qty').val()) + parseInt($('.children_qty').val());
-						//alert($('.adult_qty').val()+'----'+lookup_arr.adult.min);
-						if(total > estimated_adult){
-							$('.children_qty').val( parseInt($('.children_qty').val()) - 1 );
-						}
-					}else if(field == 'children'){
-						var total = parseInt($('.adult_qty').val()) + parseInt($('.children_qty').val());
-						if(total > lookup_arr.adult.max){
-							$('.adult_qty').val( parseInt($('.adult_qty').val()) - 1 );
-						}
-					}
-				}			
-            }else{
-                var min_val = lookup_arr[field]['min']; 
-                if(current_val > min_val){
-                    $('.'+field+'_qty').val(current_val - 1 );
-					var estimated_adult = parseInt($('.room_qty').val()) * 4;
-					if(field == 'room'){
-						if(parseInt($('.room_qty').val()) < parseInt($('.adult_qty').val())){
-							lookup_arr.adult.max = estimated_adult;
-							lookup_arr.adult.min = $('.room_qty').val();
-							lookup_arr.children.max = estimated_adult - parseInt($('.room_qty').val());
-							if(parseInt($('.adult_qty').val()) > estimated_adult){
-								$('.adult_qty').val(estimated_adult);
-							}
-							var total = parseInt($('.adult_qty').val()) + parseInt($('.children_qty').val());
-							if(total > estimated_adult){
-								$('.children_qty').val( estimated_adult - parseInt($('.adult_qty').val()) );
-							}
-						}
-					}
-				}
-            }
-        }
-    </script>
+    <script src="script.js"></script>
   </body>
 </html>
